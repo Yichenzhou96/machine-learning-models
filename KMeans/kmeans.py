31ec74a5c4943ed229f7955902f6f6494fb6bd10
Yichen <yichenzhou96@outlook.com>
2020-09-07 12:39:38 +0100

    add predict function

--- KMeans/kmeans.py
+++ KMeans/kmeans.py
@@ -1,5 +1,7 @@
 import random
 import numpy as np
+
+
 class kmeans:
     def __init__(self, k=None):
         self.k = k
@@ -9,31 +11,42 @@ class kmeans:
         points = random.sample(range(len(x)), k=self.k)
         self.means = x[points]
 
-    def calculate_distance(self, a):
+    def calculate_distance_to_means(self, a):
         return np.linalg.norm(a-self.means, axis=1)
 
+    @staticmethod
+    def calculate_distance(a):
+        return np.linalg.norm(a)
+
     def update_means(self, x):
-        labels = []
-        for instance in x:
-            distance = self.calculate_distance(instance)
-            labels.append(np.argmin(distance))
+        n = len(x)
+        labels = np.zeros((n,))
+        for i in range(n):
+            distance = self.calculate_distance_to_means(x[i])
+            labels[i] = np.argmin(distance)
 
         for i in range(self.k):
             clusters = x[[l for l, val in enumerate(labels) if val == i]]
             self.means[i] = np.mean(clusters, axis=0)
-
         return labels
 
     def fit(self, x):
         self.initialize_means_randomly(x)
+        prev = np.zeros((len(x)))
         while True:
-            prev = []
             current_labels = self.update_means(x)
-            if np.linalg.norm([a - b for a, b in zip(prev, current_labels)]) < 10e-5:
+            res = self.calculate_distance(current_labels-prev)
+            print(res)
+            if res < 10e-5:
                 break
+            prev = np.copy(current_labels)
 
     def predict(self, x):
-        pass
+        predictions = []
+        for test in x:
+            distance = self.calculate_distance_to_means(test)
+            predictions.append(np.argmin(distance))
+        return predictions
 
 
 from sklearn.model_selection import train_test_split
@@ -46,7 +59,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 
 model = kmeans(3)
 model.fit(X_train)
-
-print(model.means)
+predictions = model.predict(X_test)
+print('prediction score: {}'.format(sum(predictions == y_test)/len(y_test)))
 
 
